541d1718d9d7a912d68da754544c39957b36be4a
sofiyan <anjroumsoufian@gmail.com>
2024-01-19 23:39:06 +0100

    add signUp functionnalities

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,16 @@
+import bcryptjs from 'bcryptjs';
+import User from "../models/user.model"
 
+export const signUp=async(req,res,next)=>{
+    const{username,email,password}=req.body;
+    const hashedPassword=bcryptjs.hashSync(password,10)
+    const newUser=User.create({username,email,password:hashedPassword})
+    try {
+        await newUser.save()
+        res.status(200).json("the user created succesfully")
 
-
-export const signUp=async(req,res)=>{
-
-
+    } catch (error) {
+        next(error)
+    }
 } 
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,61 @@
-
+import {useState} from "react"
 
 function SignUp() {
+    const[error,setError]=useState(null)
+    const[loading,setLoading]=useState(false)
+
+    const [formData,setFormData]=useState({})
+
+    const handleChange=(e)=>{
+        setFormData({
+            ...formData,
+            [e.target.id]:e.target.value
+        })
+    }
+    const submitHandler=async(e)=>{
+        e.preventDefault();
+        try {
+            setLoading(true)
+            const res=await fetch("/sign-in",{
+                method:"POST",
+                body:JSON.stringify(formData),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            const data=res.json()
+            if(data.success===false){
+                setError(data.message)
+                return
+            }
+            setLoading(false)
+            setError(null)
+        } catch (error) {
+            setLoading(false)
+            setError(error.message)
+        }
+    }
     return (
-        <div>
-            
+        <div className="w-1/2 mx-auto  bg-slate-100 my-20 shadow-md">
+            <h2 className=" font-medium mx-auto  w-fit  py-5  text-lg ">sign up</h2>
+            <from onSubmit={submitHandler} className="flex flex-col items-center gap-6">
+                <input type="text" 
+                        placeholder="username" 
+                        className=" w-3/4 mx-auto p-1 rounded-md" 
+                        onChange={handleChange} />
+                <input type="email" 
+                        placeholder="email" 
+                        className=" w-3/4 mx-auto p-1 rounded-md" 
+                        onChange={handleChange} />
+                <input type="password" 
+                        placeholder="password" 
+                        className=" w-3/4 mx-auto p-1 rounded-md" 
+                        onChange={handleChange} />
+                <button disabled={loading} className="bg-gray-300 w-3/4 mb-4 p-1 hover:opacity-90">
+                    {loading? "loading..." :"sign up" }
+                </button>
+            </from>
+            {error && <p className="text-red-500"> {error} </p> }
         </div>
     )
 }
