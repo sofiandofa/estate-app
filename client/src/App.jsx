222e3162cf8e613252f34f2788e9fe9234bc4d85
sofiyan <anjroumsoufian@gmail.com>
2024-01-21 00:57:19 +0100

    add privateRoute component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import SignUp from './pages/SignUp.jsx'
 import SignIn from './pages/SignIn.jsx'
 
 import Header from './components/Header.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
+import Profile from './pages/Profile.jsx'
 
 function App() {
 
@@ -15,7 +17,9 @@ function App() {
             <Route path='/'  element={<Home/>} />
             <Route path='/sign-up'  element={<SignUp/>} />
             <Route path='/sign-in'  element={<SignIn/>} />
-
+            <Route element={<PrivateRoute/>} >
+              <Route path='/profile' element={<Profile/>} />
+            </Route>
         </Routes>
     </BrowserRouter>
   )
---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { Outlet ,Navigate} from "react-router-dom"
+import {  useSelector } from "react-redux"
+
+const PrivateRoute = () => {
+    const{currentUser}=useSelector(state=>state.user)
+
+    return currentUser ? <Outlet /> : <Navigate to='/'/> 
+    
+}
+
+export default PrivateRoute
